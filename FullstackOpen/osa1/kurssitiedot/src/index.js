c1262093b47a8a48671204f4054d7b4e7c71b081
jjtalo <54635478+jjtalo@users.noreply.github.com>
2019-09-18 10:15:50 +0300

    osa1 kurssitiedot step3

--- FullstackOpen/osa1/kurssitiedot/src/index.js
+++ FullstackOpen/osa1/kurssitiedot/src/index.js
@@ -3,38 +3,41 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const Header = (props) => {
-    return (
-    <div>
-      <h1>
-        {course}
-      </h1>>
-    </div>
-    )
+    console.log(props)
+    return <h1>{props.course}</h1>
   }
 
   const Content = (props) => {
+    console.log(props)
     return (
       <>
-	      <p> {part1} {exercises1} </p>
-	      <p> {part2} {exercises2} </p>
-	      <p> {part3} {exercises3} </p>
+	      <p> {part1.name} {part1.exercises}  </p>
+	      <p> {part2.name} {part2.exercises}  </p>
+        <p> {part3.name} {part3.exercises}  </p>
       </>
     )
   }
 
   const Total = (props) => {
+    console.log(props)
     return (
       <>
       <p>
-        Number of exercises = {exercises1 + exercises2 + exercises3}
+        Number of exercises = {part1.exercises + part2.exercises + part3.exercises}
       </p>
       </> 
     )
@@ -44,7 +47,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content />
-      <Total Number of exercises={exercises1 + exercises2 + exercises3} />
+      <Total Number of exercises />
     </div>
   )
 }
