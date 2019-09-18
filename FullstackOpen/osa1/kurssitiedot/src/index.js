af934198678029d67364bacb79519e181da3004a
jjtalo <54635478+jjtalo@users.noreply.github.com>
2019-09-18 12:54:39 +0300

    osa1 kurssitiedot step5

--- FullstackOpen/osa1/kurssitiedot/src/index.js
+++ FullstackOpen/osa1/kurssitiedot/src/index.js
@@ -2,54 +2,56 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   const Header = (props) => {
     console.log(props)
-    return <h1>{props.course}</h1>
+    return <h1>{course.name}</h1>
   }
 
   const Content = (props) => {
     console.log(props)
     return (
       <>
-      <p>{parts[0].name}, Number of exercises: {parts[0].exercises}</p>
-      <p>{parts[1].name}, Number of exercises: {parts[1].exercises}</p>
-      <p>{parts[2].name}, Number of exercises: {parts[2].exercises}</p>
+      <p>Course name: {course.parts[0].name}, Number of exercises: {course.parts[0].exercises}</p>
+      <p>Course name: {course.parts[1].name}, Number of exercises: {course.parts[1].exercises}</p>
+      <p>Course name: {course.parts[2].name}, Number of exercises: {course.parts[2].exercises}</p>
       </>
     )
   }
 
   const Total = (props) => {
     console.log(props)
-    const sum = parts[0].exercises + parts[1].exercises + parts[2].exercises
+    const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
     console.log(sum)
-    return ( 
-    <>
-      <p>Total number of exercises: {sum}</p>
-    </>
+    return (
+      <>
+        <p>Total number of exercises: {sum}</p>
+      </>
     )
   }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
