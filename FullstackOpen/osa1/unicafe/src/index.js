186838c57d02e3633a46f0cae41f8c9f482c7698
jjtalo <54635478+jjtalo@users.noreply.github.com>
2019-09-22 13:06:27 +0300

    osa1 unicafe step1

--- FullstackOpen/osa1/unicafe/src/index.js
+++ FullstackOpen/osa1/unicafe/src/index.js
@@ -1,15 +1,29 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+)
+
+
 const App = () => {
   // tallenna napit omaan tilaansa
+
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
     <div>
-      code here
+      <h1>Give Feedback</h1>
+      <p><Button handleClick={() => setGood(good + 1)} text="Good" /></p>
+      <p><Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" /></p>
+      <p><Button handleClick={() => setBad(bad + 1)} text="Bad" /></p>
+      <h1>Statistics</h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
     </div>
   )
 }
