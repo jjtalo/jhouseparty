a0c5551753a45f251802044203eacd2f78313f62
jjtalo <54635478+jjtalo@users.noreply.github.com>
2019-09-22 18:10:40 +0300

    osa1 unicafe step2

--- FullstackOpen/osa1/unicafe/src/index.js
+++ FullstackOpen/osa1/unicafe/src/index.js
@@ -13,17 +13,24 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = (good + bad + neutral)
+  const [avg, setAvg] = useState(0)
+  
+  
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <p><Button handleClick={() => setGood(good + 1)} text="Good" /></p>
-      <p><Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" /></p>
-      <p><Button handleClick={() => setBad(bad + 1)} text="Bad" /></p>
+      <p><Button handleClick={() => [setGood(good + 1), setAvg(avg + 1)]} text="Good" /></p>
+      <p><Button handleClick={() => [setNeutral(neutral + 1), setAvg(avg + 0)]} text="Neutral" /></p>
+      <p><Button handleClick={() => [setBad(bad + 1), setAvg(avg - 1)]} text="Bad" /></p>
       <h1>Statistics</h1>
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {all}</p>
+      <p>Avg {avg/all}</p>
+      <p>Positive {(good/all)*100}%</p>
     </div>
   )
 }
